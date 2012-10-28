731b31193f820e7e325db3d38f5ef90e3d34c385
Matthew Denner <matt.denner@gmail.com>
2012-10-28 11:28:28 +0000

    Problem 03: updated to use pattern matching
    Interestingly I didn't reach for this first because there's an obvious
    "when X and Y" kind of behaviour here.

--- src/main/scala/com/denner/ninetynine/Problem03.scala
+++ src/main/scala/com/denner/ninetynine/Problem03.scala
@@ -1,9 +1,9 @@
 package com.denner.ninetynine
 
 object Problem03 {
-  def nth[T](n: Int, list: List[T]): T =
-    if (n < 0)               throw new NoSuchElementException
-    else if (n == 0)         list.head
-    else if (list == List()) throw new NoSuchElementException
-    else                     nth(n-1, list.tail)
+  def nth[T](n: Int, list: List[T]): T = (n, list) match {
+    case (0, x :: List()) => x
+    case (n, _ :: tail)   => nth(n-1, tail)
+    case (_, List())      => throw new NoSuchElementException
+  }
 }
