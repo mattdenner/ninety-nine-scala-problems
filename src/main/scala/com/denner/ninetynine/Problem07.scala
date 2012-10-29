8b5879974016022fd3cf54d9bc7aa5e98057c7f6
Matthew Denner <matt.denner@gmail.com>
2012-10-29 15:25:05 +0000

    Problem 07: flatten(List(...)) implemented
    I have to admit to cheating here and looking at the answer.  flatMap was
    obvious but I couldn't get the case to work.  I didn't know you could
    type the values in a case ('case x:T').

--- src/main/scala/com/denner/ninetynine/Problem07.scala
+++ src/main/scala/com/denner/ninetynine/Problem07.scala
@@ -1,6 +1,8 @@
 package com.denner.ninetynine
 
 object Problem07 {
-  def flatten[T](listOfLists: List[List[T]]): List[T] =
-    listOfLists.foldRight(List[T]()) { (flat,list) => flat ++ list }
+  def flatten(listOfThings: List[Any]): List[Any] = listOfThings.flatMap {
+    case value: List[_] => flatten(value)
+    case value          => List(value)
+  }
 }
--- src/test/scala/com/denner/ninetynine/Problem07Spec.scala
+++ src/test/scala/com/denner/ninetynine/Problem07Spec.scala
@@ -10,9 +10,14 @@ class Problem07Spec extends Specification {
     "List(List())"         in { flatten(List(List()))         === List() }
     "List(List(), List())" in { flatten(List(List(), List())) === List() }
 
+    "List(1, List(2))" in { flatten(List(1, List(2))) === List(1,2) }
+    "List(List(1), 2)" in { flatten(List(List(1), 2)) === List(1,2) }
+
     "List(List(1), List())"      in { flatten(List(List(1), List()))      === List(1) }
     "List(List(), List(1))"      in { flatten(List(List(), List(1)))      === List(1) }
     "List(List(1), List(2))"     in { flatten(List(List(1), List(2)))     === List(1,2) }
     "List(List(1,2), List(3,4))" in { flatten(List(List(1,2), List(3,4))) === List(1,2,3,4) }
+
+    "List(List(List(1), 2), 3)" in { flatten(List(List(List(1), 2), 3)) === List(1,2,3) }
   }
 }
