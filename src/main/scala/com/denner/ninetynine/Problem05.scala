b9b3e198620f5c151169137c4f77003c552a92b1
Matthew Denner <matt.denner@gmail.com>
2012-10-29 14:33:14 +0000

    Problem 05: isPalindrome(List(...)) updated
    Duh, reverse is the same as the normal!

--- src/main/scala/com/denner/ninetynine/Problem05.scala
+++ src/main/scala/com/denner/ninetynine/Problem05.scala
@@ -1,6 +1,5 @@
 package com.denner.ninetynine
 
 object Problem05 {
-  def isPalindrome[T](list: List[T]): Boolean =
-    list.zip(list.reverse).forall { case (x,y) => x == y }
+  def isPalindrome[T](list: List[T]): Boolean = list == list.reverse
 }
